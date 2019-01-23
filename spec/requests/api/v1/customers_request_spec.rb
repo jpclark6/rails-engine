e792da753b63c48b637b30c409dd78f435bc9848
jpclark6 <jpclark6@gmail.com>
2019-01-22 20:37:47 -0700

    adds json customer index test

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -5,6 +5,17 @@ describe "Customers API" do
     create_list(:customer, 3)
 
     get '/api/v1/customers'
+
     expect(response).to be_successful
+    
+    expect(response.body["data"].length).to eq(3)
+
+    expect(response.body["data"][0]["first_name"]).to eq(Customer.all.first.first_name)
+    expect(response.body["data"][1]["first_name"]).to eq(Customer.all.second.first_name)
+    expect(response.body["data"][2]["first_name"]).to eq(Customer.all.third.first_name)
+
+    expect(response.body["data"][0]["last_name"]).to eq(Customer.all.first.last_name)
+    expect(response.body["data"][1]["last_name"]).to eq(Customer.all.second.last_name)
+    expect(response.body["data"][2]["last_name"]).to eq(Customer.all.third.last_name)
   end
 end
