7ff2900935a9344e913344d0a07960608609f8df
jpclark6 <jpclark6@gmail.com>
2019-01-23 10:50:05 -0700

    updates to serializers, customer index test passing

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CustomersController < ApplicationController
   def index
-    
+    render json: CustomerSerializer.new(Customer.all)
   end
 end
--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -9,15 +9,15 @@ describe "Customers API" do
     expect(response).to be_successful
 
     customers_data = JSON.parse(response.body)
-    expect(customers_data[0]["data"].length).to eq(3)
+    expect(customers_data["data"].length).to eq(3)
 
-    expect(customers_data[0]["data"][0]["first_name"]).to eq(Customer.all.first.first_name)
-    expect(customers_data[0]["data"][1]["first_name"]).to eq(Customer.all.second.first_name)
-    expect(customers_data[0]["data"][2]["first_name"]).to eq(Customer.all.third.first_name)
+    expect(customers_data["data"][0]["attributes"]["first_name"]).to eq(Customer.all.first.first_name)
+    expect(customers_data["data"][1]["attributes"]["first_name"]).to eq(Customer.all.second.first_name)
+    expect(customers_data["data"][2]["attributes"]["first_name"]).to eq(Customer.all.third.first_name)
 
-    expect(customers_data[0]["data"][0]["last_name"]).to eq(Customer.all.first.last_name)
-    expect(customers_data[0]["data"][1]["last_name"]).to eq(Customer.all.second.last_name)
-    expect(customers_data[0]["data"][2]["last_name"]).to eq(Customer.all.third.last_name)
+    expect(customers_data["data"][0]["attributes"]["last_name"]).to eq(Customer.all.first.last_name)
+    expect(customers_data["data"][1]["attributes"]["last_name"]).to eq(Customer.all.second.last_name)
+    expect(customers_data["data"][2]["attributes"]["last_name"]).to eq(Customer.all.third.last_name)
   end
   it "sends data on one customer" do
     create_list(:customer, 3)
@@ -27,18 +27,19 @@ describe "Customers API" do
     expect(response).to be_successful
 
     customers_data = JSON.parse(response.body)
-    expect(customers_data[0].length).to eq(1)
-    expect(customers_data[0]["data"].length).to eq(3)
-    expect(customers_data[0]["data"]["type"].length).to eq(1)
-    expect(customers_data[0]["data"]["attributes"].length).to eq(2)
 
-    expect(customers_data[0]["data"][0]["id"]).to eq(Customer.first.id)
-    expect(customers_data[0]["data"][0]["type"]).to eq("customer")
-    expect(customers_data[0]["data"][0]["attributes"]["first_name"]).to eq(Customer.first.first_name)
-    expect(customers_data[0]["data"][0]["attributes"]["last_name"]).to eq(Customer.first.last_name)
+    expect(customers_data.length).to eq(1)
+    expect(customers_data["data"].length).to eq(3)
 
-    expect(customers_data[0]["data"][1]["id"]).to eq(Customer.second.id)
-    expect(customers_data[0]["data"][2]["id"]).to eq(Customer.third.id)
+    expect(customers_data["data"][0]["id"]).to eq(Customer.first.id.to_s)
+    expect(customers_data["data"][0]["type"]).to eq("customer")
+    expect(customers_data["data"][0]["attributes"]["first_name"]).to eq(Customer.first.first_name)
+    expect(customers_data["data"][0]["attributes"]["last_name"]).to eq(Customer.first.last_name)
+
+    expect(customers_data["data"][1]["id"]).to eq(Customer.second.id.to_s)
+    expect(customers_data["data"][1].length).to eq(3)
+    expect(customers_data["data"][2]["id"]).to eq(Customer.third.id.to_s)
+    expect(customers_data["data"][2].length).to eq(3)
 
   end
 end
